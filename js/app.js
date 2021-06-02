f152166534228ddc2e43edd20b15cf9e82081f28
Aurelien Lebreton <lebreton97480@gmail.com>
2021-06-02 09:34:56 +0200

    Fix Js

--- js/app.js
+++ js/app.js
@@ -12,65 +12,12 @@
     //=============OU la il u à truc chelou dans se js ???
     const Xcode = "БнубнуϮДнрлЯтгурϮЈϷϮАзгмфгмугϮвЯмсϮлнмϮбнвгϼϮИгϮлгϮорҷсгмтгϮлнзϮбϵгстϮИгБнвгКгСнзрϮϯ"
 
-    //=== Router
-    //Config
-    const Router = {
-        default: 'pages/home.html',
-        route: '/',
-        footer: 'pages/footer.html',
-        pages: [
-            {
-                name: 'home',
-                path: 'pages/home.html',
-                visibleInMenu: true,
-            },
-            {
-                name: 'Galery Photo',
-                path: 'pages/galeryphoto.html',
-                visibleInMenu: true,
-            },
-            {
-                name: 'Loupy Falcon 9',
-                author: 'Pierre',
-                path: 'team/loupy/index.html',
-                iframe: true,
-                visibleInMenu: true,
-                isFont: true,
-            },
-            {
-                name: 'Shop',
-                author: 'kévin',
-                path: 'team/kévin/shop.html',
-                iframe: true,
-                visibleInMenu: true,
-                isFont: true,
-            },
-            {
-                name: 'Careers',
-                author: 'kévin',
-                path: 'team/kévin/careers.html',
-                iframe: true,
-                visibleInMenu: true,
-                isFont: true,
-            },
-            {
-                name: 'Goodies',
-                author: 'kévin',
-                path: 'team/kévin/goodies.html',
-                iframe: true,
-                visibleInMenu: true,
-                isFont: true,
-            }
-        ]
-    }
-
     const GetPage = async (url) => {
         const getPagebyFetch = await fetch(url)
         const html = await getPagebyFetch.text()
 
         return html
     }
-
 
     const GetFooter = async (route) => {
         var footer_html = await GetPage(Router.footer)
